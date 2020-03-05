93c83a2f03bb0577aff6155ad552f75d1bb7da52
Nathaly <natharb09@gmail.com>
2020-03-05 14:45:16 -0500

    feat: adding build script and home template

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+  const view = `
+    <div class="characters">
+      <article class="characters__item">
+        <a href="#/1/">
+          <img src="image" alt="character image" />
+          <h2>Character Name</h2>
+        </a>
+      </article>
+    </div>
+  `;
+  return view;
+};
+
+export default Home;
