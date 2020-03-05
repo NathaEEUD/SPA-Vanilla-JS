9afbc58806ec1cb66f6a66a1d6f9a236750d47d2
Nathaly <natharb09@gmail.com>
2020-03-05 16:33:44 -0500

    feat: implementing application router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
     <div class="characters">
       <article class="characters__item">
         <a href="#/1/">
-          <img src="image" alt="character image" />
+          <img src="image" alt="character image" >
           <h2>Character Name</h2>
         </a>
       </article>
